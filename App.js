20a037f6e166acb61d67d5a4e83d1303f32930f5
Harshdeep Singh <56130865+coderhersh@users.noreply.github.com>
2022-12-10 14:51:41 +0530

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import Display from './Components/Display';
-
-export default function App() {
-  return (
-    <div className='flex h-screen pt-20 justify-center bg-teal-800'>
-      <Display/>
-    </div>
-  );
-}
