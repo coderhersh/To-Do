fd12b8549361581302d0a824658919791aa07b31
Harshdeep Singh <56130865+coderhersh@users.noreply.github.com>
2022-12-10 14:47:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import Display from './Components/Display';
+
+export default function App() {
+  return (
+    <div className='flex h-screen pt-20 justify-center bg-teal-800'>
+      <Display/>
+    </div>
+  );
+}
